65e1a9707ce6f9d051e94879fbca300e6bb6dfb6
Tejas <shindetejas2011@gmail.com>
2021-07-28 23:57:22 +0530

    added index.js file and implemeted keystroke and sound functionality

---
+++ drum-kit/index.js
@@ -0,0 +1,18 @@
+window.addEventListener('keydown', playAudio);
+
+function playAudio(event) {
+    const audioElement = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    if (!audioElement) return;
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    key.classList.add('playing');
+    audioElement.currentTime = 0;
+    audioElement.play();
+}
+
+const keysArray = document.querySelectorAll('.key');
+keysArray.forEach(key => key.addEventListener('transitionend', removeClass));
+
+function removeClass(event) {
+    if (event.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
